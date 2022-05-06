baee951b00d904e522f0aa4092345cf648a1f93a
carmelo <carmeload@gmail.com>
2022-05-06 20:55:13 +0200

    Hardcoding the header

--- header.php
+++ header.php
@@ -25,24 +25,48 @@
 <div id="page" class="site">
 	<header id="masthead" class="site-header">
 		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-			<div class="container">
-				<?php the_custom_logo(); ?>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="main-menu">
-				<?php
-		            wp_nav_menu(array(
-        		        'theme_location' => 'main-menu',
-                		'container' => false,
-                		'menu_class' => 'ja-menu',
-                		'fallback_cb' => '__return_false',
-                		'items_wrap' => '<ul id="%1$s" class="navbar-nav ms-auto mb-2 mb-md-0 %2$s">%3$s</ul>',
-                		'depth' => 2,
-                		'walker' => new bootstrap_5_wp_nav_menu_walker()
-            		));
-            	?>
-				</div>
-		</div>
+  			<div class="container">
+    			<a class="navbar-brand" href="jardinactivo.com"><img  src="http://localhost/jardinactivo/wp-content/themes/jardinactivo/assets/img/logotipojardinactivo.png"></a>
+    			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      				<span class="navbar-toggler-icon"></span>
+    			</button>
+    			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+      				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+				        <li class="nav-item">
+          					<a class="nav-link" aria-current="page" href="#">Home</a>
+        				</li>
+				        <li class="nav-item">
+          					<a class="nav-link" href="#">Link</a>
+        				</li>
+        				<li class="nav-item dropdown">
+          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            					Dropdown
+          					</a>
+          					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            					<li><a class="dropdown-item" href="#">Action</a></li>
+            					<li><a class="dropdown-item" href="#">Another action</a></li>
+            					<li><hr class="dropdown-divider"></li>
+            					<li><a class="dropdown-item" href="#">Something else here</a></li>
+          					</ul>
+        				</li>
+        				<li class="nav-item dropdown">
+          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">login</a>
+							<ul class="dropdown-menu m-0 p-2 bg-primary " aria-labelledby="navbarDropdown">
+            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Usuario" aria-label="Search"></li>
+            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Contraseña" aria-label="Search"></li>
+            						<li class="d-flex justify-content-center p-1"><button class="btn btn-outline-success bg-light" type="submit">Acceder</button></li>
+							</ul>
+ 		       			</li>
+							<li class="nav-item dropdown">
+          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">login</a>
+							<ul class="dropdown-menu m-0 p-2 bg-primary " aria-labelledby="navbarDropdown">
+            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Usuario" aria-label="Search"></li>
+            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Contraseña" aria-label="Search"></li>
+            						<li class="d-flex justify-content-center p-1"><button class="btn btn-outline-success bg-light" type="submit">Acceder</button></li>
+							</ul>
+ 		       			</li>
+
+					</div>
+  			</div>
 		</nav>
 	</header><!-- #masthead -->
