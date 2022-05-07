211af7566acb18fd1fbc3ec1e1b45a427b1dc341
carmelo <carmeload@gmail.com>
2022-05-07 18:16:50 +0200

    crete both files to menu, logged in and not logged in

--- header.php
+++ header.php
@@ -35,41 +35,14 @@
     			</button>
     			<div class="collapse navbar-collapse" id="navbarSupportedContent">
       				<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-				        <li class="nav-item">
-          					<a class="nav-link" aria-current="page" href="#">Home</a>
-        				</li>
-				        <li class="nav-item">
-          					<a class="nav-link" href="#">Link</a>
-        				</li>
-        				<li class="nav-item dropdown">
-          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            					Dropdown
-          					</a>
-          					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            					<li><a class="dropdown-item" href="#">Action</a></li>
-            					<li><a class="dropdown-item" href="#">Another action</a></li>
-            					<li><hr class="dropdown-divider"></li>
-            					<li><a class="dropdown-item" href="#">Something else here</a></li>
-          					</ul>
-        				</li>
-        				<li class="nav-item dropdown">
-          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">login</a>
-							<ul class="dropdown-menu m-0 p-2 bg-primary " aria-labelledby="navbarDropdown">
-            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Usuario" aria-label="Search"></li>
-            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Contraseña" aria-label="Search"></li>
-            						<li class="d-flex justify-content-center p-1"><button class="btn btn-outline-success bg-light" type="submit">Acceder</button></li>
-							</ul>
- 		       			</li>
-							<li class="nav-item dropdown">
-          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">login</a>
-							<ul class="dropdown-menu m-0 p-2 bg-primary " aria-labelledby="navbarDropdown">
-            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Usuario" aria-label="Search"></li>
-            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Contraseña" aria-label="Search"></li>
-            						<li class="d-flex justify-content-center p-1"><button class="btn btn-outline-success bg-light" type="submit">Acceder</button></li>
-							</ul>
- 		       			</li>
-
-					</div>
+					<?php
+						if (is_user_logged_in())
+							require get_template_directory() . '/inc/menu-logged-in.php';
+						else
+							require get_template_directory() . '/inc/menu-not-logged-in.php';
+					?>
+					</ul>
+				</div>
   			</div>
 		</nav>
 	</header><!-- #masthead -->
---
+++ inc/menu-not-logged-in.php
@@ -0,0 +1,25 @@
+                        <li class="nav-item">
+          					<a class="nav-link" aria-current="page" href="#">Home</a>
+        				</li>
+				        <li class="nav-item">
+          					<a class="nav-link" href="#">Link</a>
+        				</li>
+        				<li class="nav-item dropdown">
+          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            					Dropdown
+          					</a>
+          					<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            					<li><a class="dropdown-item" href="#">Action</a></li>
+            					<li><a class="dropdown-item" href="#">Another action</a></li>
+            					<li><hr class="dropdown-divider"></li>
+            					<li><a class="dropdown-item" href="#">Something else here</a></li>
+          					</ul>
+        				</li>
+        				<li class="nav-item dropdown">
+          					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">login</a>
+							<ul class="dropdown-menu m-0 p-2 bg-primary " aria-labelledby="navbarDropdown">
+            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Usuario" aria-label="Search"></li>
+            						<li class="p-1"><input class="form-control me-2" type="search" placeholder="Contraseña" aria-label="Search"></li>
+            						<li class="d-flex justify-content-center p-1"><button class="btn btn-outline-success bg-light" type="submit">Acceder</button></li>
+							</ul>
+ 		       			</li>
