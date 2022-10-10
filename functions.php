3bcd3c26f5c4d0e94dd5678226e476d4b45a7e31
carmelo <carmeload@gmail.com>
2022-10-10 20:37:54 +0200

    starting with full site editin for beginig

--- functions.php
+++ functions.php
@@ -15,6 +15,8 @@
  */
 define( 'JARDINACTIVO_VERSION', wp_get_theme()->get( 'Version' ) );
 
+
+
 /**
  * Add theme support for block styles and editor style.
  *
@@ -24,23 +26,6 @@ define( 'JARDINACTIVO_VERSION', wp_get_theme()->get( 'Version' ) );
  */
 function jardinactivo_setup() {
 	add_theme_support( 'wp-block-styles' );
-	add_editor_style( './assets/css/style-shared.min.css' );
-
-	/*
-	 * Load additional block styles.
-	 * See details on how to add more styles in the readme.txt.
-	 */
-	$styled_blocks = [ 'button', 'file', 'latest-comments', 'latest-posts', 'quote', 'search' ];
-	foreach ( $styled_blocks as $block_name ) {
-		$args = array(
-			'handle' => "jardinactivo-$block_name",
-			'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.min.css" ),
-			'path'   => get_theme_file_path( "assets/css/blocks/$block_name.min.css" ),
-		);
-		// Replace the "core" prefix if you are styling blocks from plugins.
-		wp_enqueue_block_style( "core/$block_name", $args );
-	}
-
 }
 add_action( 'after_setup_theme', 'jardinactivo_setup' );
 
@@ -66,15 +51,3 @@ function jardinactivo_styles() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'jardinactivo_styles' );
-
-// Filters.
-require_once get_theme_file_path( 'inc/filters.php' );
-
-// Block variation example.
-require_once get_theme_file_path( 'inc/register-block-variations.php' );
-
-// Block style examples.
-require_once get_theme_file_path( 'inc/register-block-styles.php' );
-
-// Block pattern and block category examples.
-require_once get_theme_file_path( 'inc/register-block-patterns.php' );
